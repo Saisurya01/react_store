2ab10d3fc801fca2e71d1822099a0951f98dc685
saisurya01 <saisuryakosuri@gmail.com>
2025-12-09 07:10:58 +0530

    deploying

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/basic_react_page/",
+  base:"/react_store/",
   plugins: [react()],
 })
